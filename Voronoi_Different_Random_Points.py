c6ef71d41d7c039ed7d8d565cd971bf04557e4fc
NejdetBalkir <nejdetbalkirgoka@gmail.com>
2024-04-23 16:49:18 +0300

    Number of different Voronoi diagrams are increased to 100 to obtain the convergence plot

--- Voronoi_Different_Random_Points.py
+++ Voronoi_Different_Random_Points.py
@@ -9,7 +9,7 @@ NoP = 100 # number of points in each Voronoi cell
 # np.random.seed(10)
 dist_to_original_storage ={}
 
-Number_of_Dif_Voronoi_Diagrams = 10
+Number_of_Dif_Voronoi_Diagrams = 100
 
 
 for i in range(Number_of_Dif_Voronoi_Diagrams):
@@ -57,29 +57,29 @@ mean_of_mean_storage_summation = [number/Number_of_Dif_Voronoi_Diagrams for numb
 # print(mean_storage)
 # np.savetxt('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/convergence_type_trial/array.txt', mean_of_mean_storage_summation, fmt='%.15f')
 
-fig1,ax1 = plt.subplots()
+# fig1,ax1 = plt.subplots()
 
 iteration = np.arange(1,101,1)
-print(iteration)
+# print(iteration)
 
-for i in range(len(mean_storage)):
-    ax1.loglog(iteration,mean_storage[f'Random_Voronoi_{i}'],label=f'Random_Voronoi_{i}')
-ax1.set_xlabel('Iteration')
-ax1.set_ylabel('Mean distance from original')
-ax1.set_title('Log-Log plot of Convergence of Each Voronoi diagrams')
+# for i in range(len(mean_storage)):
+#     ax1.loglog(iteration,mean_storage[f'Random_Voronoi_{i}'],label=f'Random_Voronoi_{i}')
+# ax1.set_xlabel('Iteration')
+# ax1.set_ylabel('Mean distance from original')
+# ax1.set_title('Log-Log plot of Convergence of Each Voronoi diagrams')
 
-fig1.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/Covergence_of_Each_Voronoi_Diagram.png', dpi=300)
+# fig1.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/Covergence_of_Each_Voronoi_Diagram.png', dpi=300)
 
 
 
-fig2,ax2 = plt.subplots()
+# fig2,ax2 = plt.subplots()
 
-ax2.loglog(iteration,mean_of_mean_storage_summation,label='Mean of the Error of all Voronoi diagrams')
-ax2.set_xlabel('Iteration')
-ax2.set_ylabel('Mean distance from Original Seed Points')
-ax2.set_title('Log-Log plot of the Mean of the Error of all Voronoi diagrams')
+# ax2.loglog(iteration,mean_of_mean_storage_summation,label='Mean of the Error of all Voronoi diagrams')
+# ax2.set_xlabel('Iteration')
+# ax2.set_ylabel('Mean distance from Original Seed Points')
+# ax2.set_title('Log-Log plot of the Mean of the Error of all Voronoi diagrams')
 
-fig2.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/Log_Log_Convergence.png',dpi=300)
+# fig2.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/Log_Log_Convergence.png',dpi=300)
 
 
 
@@ -89,17 +89,17 @@ ax3.set_xlabel('Iteration')
 ax3.set_ylabel('Mean Disrance from Original Seed Points')
 ax3.set_title('Semilog plot of the Mean of the Error of all Voronoi diagrams')
 
-fig3.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/semilog_Convergence.png',dpi=300)
+fig3.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Inverse_Voronoi/Plots/semilog_Convergence.png',dpi=300)
 
 
-fig4,ax4 = plt.subplots()
-ax4.plot(iteration,mean_of_mean_storage_summation,label='Mean of the Error of all Voronoi diagrams')
-ax4.set_xlabel('Iteration')
-ax4.set_ylabel('Mean Disrance from Original Seed Points')
-ax4.set_title('Plot of the Mean of the Error of all Voronoi diagrams')
-fig4.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/plot_Convergence.png',dpi=300)
+# fig4,ax4 = plt.subplots()
+# ax4.plot(iteration,mean_of_mean_storage_summation,label='Mean of the Error of all Voronoi diagrams')
+# ax4.set_xlabel('Iteration')
+# ax4.set_ylabel('Mean Disrance from Original Seed Points')
+# ax4.set_title('Plot of the Mean of the Error of all Voronoi diagrams')
+# fig4.savefig('/Users/balkirgoka/Documents/Phd/Projects/Voronoi/Voronoi_6/Plots/plot_Convergence.png',dpi=300)
 
-plt.show()
+# plt.show()
 
 
 
